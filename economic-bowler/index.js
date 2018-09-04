24d45d2389d86c3ac79460084206793b1c8ef10f
guru-sode <gurukiran.sd@mountblue.io>
2018-09-04 14:05:04 +0530

    Final version

---
+++ economic-bowler/index.js
@@ -0,0 +1,60 @@
+var res={};
+var sum=0;
+var fs = require('fs');
+var readmeDelivery = fs.readFileSync('deliveries.json', 'utf8');
+var deliverObj = JSON.parse(readmeDelivery);
+
+
+var idObject=[];
+var readmeMatches=fs.readFileSync('matches.json','utf8');
+var MatchesObj=JSON.parse(readmeMatches);
+MatchesObj.forEach(function(match){
+  if(match["season"]==2015)
+  idObject.push(match["id"]);
+});
+
+var idDelivery={};
+deliverObj.forEach(function(delivery){
+  idObject.forEach(function(id){
+    if(delivery["match_id"]==id){
+    idDelivery=delivery;
+    if(idDelivery["bowler"] in res){
+      res[idDelivery["bowler"]]=parseInt(res[idDelivery["bowler"]])+parseInt(idDelivery["total_runs"]);
+    }
+    else{
+      res[idDelivery["bowler"]]=0;
+    }
+  }
+  });
+});
+
+var NoBowls={};
+deliverObj.forEach(function(delivery){
+  idObject.forEach(function(id){
+    if(delivery["match_id"]==id){
+    idDelivery=delivery;
+  if(delivery["bowler"] in NoBowls){
+    NoBowls[delivery["bowler"]]++;
+  }
+  else{
+    NoBowls[delivery["bowler"]]=0;
+  }
+}
+});
+});
+var ans={};
+var runs=Object.values(res);
+var bowlerName=Object.keys(res);
+var bowls=Object.values(NoBowls);
+var eco=[];
+for(var i=0;i<runs.length;i++){
+  eco.push(runs[i]/bowls[i]);
+}
+
+var ans={};
+for(var i=0;i<bowlerName.length;i++){
+  ans[bowlerName[i]]=eco[i];
+}
+console.log(ans);
+
+fs.writeFileSync('economy.json',JSON.stringify(ans));
