98a64838de1d45e374abc2b35923d2596aeb7229
guru-sode <gurukiran.sd@mountblue.io>
2018-09-08 11:35:46 +0530

    Use map reduce

--- number-of-matches-played/match-read.js
+++ number-of-matches-played/match-read.js
@@ -1,17 +1,16 @@
 
-var fs=require('fs');
-var count={};
-
-var readme=fs.readFileSync('matches.json','utf8');
-var obj=JSON.parse(readme);
+let fs=require('fs');
+let readme=fs.readFileSync('matches.json','utf8');
+let obj=JSON.parse(readme);
 
 module.exports.readingMatch=function(obj){
-  obj.map(ele=>{
-    if(ele["season"] in count)
-    count[ele["season"]]++;
+  let count= obj.reduce((acc,cur)=>{
+    if(cur["season"] in acc)
+    acc[cur["season"]]++;
     else
-    count[ele["season"]]=1;
-  });
-  fs.writeFileSync('match-count.json',JSON.stringify(count));
+    acc[cur["season"]]=1;
+    return acc;
+},{}); 
   return count;
 };
+fs.writeFileSync('match-count.json',JSON.stringify(this.readingMatch(obj)));
